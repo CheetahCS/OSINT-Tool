52cc7848d9db46691c1483195db9e8e83c6917a9
CheetahCS <oliver.mik88@gmail.com>
2025-06-05 13:49:49 +0100

    Changed the email breaches attribute to be of type dictionary

--- src/target.py
+++ src/target.py
@@ -1,6 +1,8 @@
 class Target:
-    def __init__(self, first_name: str = None, surname: str = None, email_address: str = None):
-        
+    def __init__(self, target_name: str = "t1",first_name: str = None, surname: str = None, email_address: str = None):
+
+        self.target_name = target_name
+
         # Basic contact info
         self.first_name = first_name
         self.surname = surname
@@ -9,7 +11,7 @@ class Target:
         self.phone_number = None
 
         # OSINT findings
-        self.email_breaches: list = []
+        self.email_breaches: dict = {}
         self.social_media_profiles: dict = {}
         self.whois_info: dict = {}
         self.dns_records: dict = {}
