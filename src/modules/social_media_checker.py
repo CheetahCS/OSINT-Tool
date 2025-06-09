c2e6cee0c216ff27f9eb1c50db3d7eed1431820a
CheetahCS <oliver.mik88@gmail.com>
2025-06-09 23:09:45 +0100

    Wrote the base functions for finding social media accounts

---
+++ src/modules/social_media_checker.py
@@ -0,0 +1,16 @@
+import target
+
+def find_socials(t: target):
+    check_by_names()
+    check_by_email()
+    check_by_phone_number()
+    pass
+
+def check_by_names():
+    pass
+
+def check_by_email():
+    pass
+
+def check_by_phone_number():
+    pass
